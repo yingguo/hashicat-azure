c8b22be150ee879cd5cd00619d01b1de0e39c9ef
yingguo <yingguo1010@yahoo.ca>
2022-10-03 20:30:39 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/Ying-training/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "myresourcegroup"
+  resource_group_name = "${var.prefix}-workshop"
 }
